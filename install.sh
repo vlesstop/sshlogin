5b0626fd603c1372c28fcd2d8701c7ef57e37954
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:32:24 +0800

    空值提醒

--- install.sh
+++ install.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-token=""
-id=""
-
 # 循环直到用户输入了有效的 token 和 id
-while [ -z "$token" ] || [ -z "$id" ]; do
+while true; do
     # 提示用户输入 Telegram Bot Token
     read -p "请输入 Telegram Bot Token: " token
 
+    # 如果用户提供了有效的 token，跳出循环
+    if [ -n "$token" ]; then
+        break
+    fi
+
+    # 用户未提供有效的 token，提示并重新输入
+    echo "请输入有效的 Token。"
+done
+
+while true; do
     # 提示用户输入 Telegram Chat ID
     read -p "请输入 Telegram Chat ID: " id
 
-    # 如果用户没有提供 token 或 id，则重复提示
-    if [ -z "$token" ] || [ -z "$id" ]; then
-        echo "请提供有效的 Token 和 Chat ID。"
+    # 如果用户提供了有效的 id，跳出循环
+    if [ -n "$id" ]; then
+        break
     fi
+
+    # 用户未提供有效的 id，提示并重新输入
+    echo "请输入有效的 Chat ID。"
 done
 
 # 创建目录 /etc/profile.d（如果不存在）
