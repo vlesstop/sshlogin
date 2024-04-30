6666e91e3dfe16f242dbe8a227203ce590985849
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:19:08 +0800

    测试

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 检查是否提供了必要的参数
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <token> <chat_id>"
-    exit 1
-fi
+# 提示用户输入 Telegram Bot Token
+read -p "请输入 Telegram Bot Token: " token
+
+# 提示用户输入 Telegram Chat ID
+read -p "请输入 Telegram Chat ID: " id
 
 # 下载脚本并给予执行权限
 curl -sL https://raw.githubusercontent.com/vlesstop/sshlogin/main/script.sh -o ssh_login_alert.sh
 chmod +x ssh_login_alert.sh
 
 # 运行脚本并传递参数
-./ssh_login_alert.sh $1 $2
+./ssh_login_alert.sh "$token" "$id"
