d899f115ff8ab4fbaacab1bb9e531d0988797c01
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:33:53 +0800

    字符

--- install.sh
+++ install.sh
@@ -3,28 +3,28 @@
 # 循环直到用户输入了有效的 token 和 id
 while true; do
     # 提示用户输入 Telegram Bot Token
-    read -p "请输入 Telegram Bot Token: " token
+    read -p "请输入 Telegram Bot Token (至少8个字符): " token
 
     # 如果用户提供了有效的 token，跳出循环
-    if [ -n "$token" ]; then
+    if [ ${#token} -ge 8 ]; then
         break
     fi
 
     # 用户未提供有效的 token，提示并重新输入
-    echo "请输入有效的 Token。"
+    echo "Token 长度不足，请至少输入 8 个字符。"
 done
 
 while true; do
     # 提示用户输入 Telegram Chat ID
-    read -p "请输入 Telegram Chat ID: " id
+    read -p "请输入 Telegram Chat ID (至少8个字符): " id
 
     # 如果用户提供了有效的 id，跳出循环
-    if [ -n "$id" ]; then
+    if [ ${#id} -ge 8 ]; then
         break
     fi
 
     # 用户未提供有效的 id，提示并重新输入
-    echo "请输入有效的 Chat ID。"
+    echo "Chat ID 长度不足，请至少输入 8 个字符。"
 done
 
 # 创建目录 /etc/profile.d（如果不存在）
