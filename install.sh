5f47ce8765adf16f4631e7fa35b9b5ef1495ff11
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:30:50 +0800

    重复提醒

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# 提示用户输入 Telegram Bot Token
-read -p "请输入 Telegram Bot Token: " token
+token=""
+id=""
 
-# 提示用户输入 Telegram Chat ID
-read -p "请输入 Telegram Chat ID: " id
+# 循环直到用户输入了有效的 token 和 id
+while [ -z "$token" ] || [ -z "$id" ]; do
+    # 提示用户输入 Telegram Bot Token
+    read -p "请输入 Telegram Bot Token: " token
+
+    # 提示用户输入 Telegram Chat ID
+    read -p "请输入 Telegram Chat ID: " id
+
+    # 如果用户没有提供 token 或 id，则重复提示
+    if [ -z "$token" ] || [ -z "$id" ]; then
+        echo "请提供有效的 Token 和 Chat ID。"
+    fi
+done
 
 # 创建目录 /etc/profile.d（如果不存在）
 sudo mkdir -p /etc/profile.d
