09e63a57ad5880080dcad69a5587c1f88fa69f82
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:28:02 +0800

    更改存放位置

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ read -p "请输入 Telegram Bot Token: " token
 # 提示用户输入 Telegram Chat ID
 read -p "请输入 Telegram Chat ID: " id
 
-
 # 下载脚本并给予执行权限
-curl -sL https://raw.githubusercontent.com/vlesstop/sshlogin/main/script.sh -o ssh_login_alert.sh
-chmod +x ssh_login_alert.sh
+sudo curl -sL https://raw.githubusercontent.com/vlesstop/sshlogin/main/script.sh -o /etc/profile.d/ssh_login_alert.sh
+sudo chmod +x /etc/profile.d/ssh_login_alert.sh
 
 # 运行脚本并传递参数
-./ssh_login_alert.sh "$token" "$id"
+/etc/profile.d/ssh_login_alert.sh "$token" "$id"
