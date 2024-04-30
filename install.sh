c2a628d83e48b12d80743e1369d426ed5387b259
vlesstop <48223192+vlesstop@users.noreply.github.com>
2024-05-01 01:20:31 +0800

    上传

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ read -p "请输入 Telegram Bot Token: " token
 # 提示用户输入 Telegram Chat ID
 read -p "请输入 Telegram Chat ID: " id
 
+
 # 下载脚本并给予执行权限
 curl -sL https://raw.githubusercontent.com/vlesstop/sshlogin/main/script.sh -o ssh_login_alert.sh
 chmod +x ssh_login_alert.sh
